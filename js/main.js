534b9d15ca5f2f8ccca2bcf690ad6e96f76bf62f
Libia Perez <alexandra220790@gmail.com>
2021-06-23 22:02:34 -0300

    Cambiando el concepto de la pagina a adopcion de perritos, cambio de paleta de colores, estilizacion del slider modal

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
-const modal = document.querySelector(".modal");
+const modal1 = document.querySelector(".modal1");
+const modal2 = document.querySelector(".modal2");
+const modal3 = document.querySelector(".modal3");
 const button1 = document.querySelector(".button1");
 button1.addEventListener("click", () => {
-  modal.classList.remove("hidden");
-  modal.classList.add("visible");
+  modal1.classList.remove("hidden");
+  modal1.classList.add("visible");
 });
 const button2 = document.querySelector(".button2");
 button2.addEventListener("click", () => {
-  modal.classList.remove("hidden");
-  modal.classList.add("visible");
+  modal2.classList.remove("hidden");
+  modal2.classList.add("visible");
 });
 const button3 = document.querySelector(".button3");
 button3.addEventListener("click", () => {
-  modal.classList.remove("hidden");
-  modal.classList.add("visible");
+  modal3.classList.remove("hidden");
+  modal3.classList.add("visible");
 });
-const closeButton = document.querySelector(".closeButton");
-closeButton.addEventListener("click", () => {
-  modal.classList.add("hidden");
-  modal.classList.remove("visible");
+const closeButton1 = document.querySelector(".closeButton1");
+closeButton1.addEventListener("click", () => {
+  modal1.classList.add("hidden");
+  modal1.classList.remove("visible");
+});
+const closeButton2 = document.querySelector(".closeButton2");
+closeButton2.addEventListener("click", () => {
+  modal2.classList.add("hidden");
+  modal2.classList.remove("visible");
+});
+
+const closeButton3 = document.querySelector(".closeButton3");
+closeButton3.addEventListener("click", () => {
+  modal3.classList.add("hidden");
+  modal3.classList.remove("visible");
 });
